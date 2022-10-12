615c5824c773795257337e6f7b846f6751948e20
GlitterGamer <102000622+GlitterGamer@users.noreply.github.com>
2022-10-11 20:02:50 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+console.log("hello")
+var firstname="Naysa"
+var lastname="Mishra"
+console.log(firstname.concat(lastname))
+var fullname="Naysa Mishra"
+console.log(fullname.length)
+var Name_1="I am Naysa Mishra"
+var replace_name=Name_1.replace("Naysa","Naksh")
+console.log(replace_name)
+var capital_name="SANJAY MISHRA"
+var lowercase= capital_name.toLowerCase()
+console.log(lowercase)
+var uppercase= lowercase.toUpperCase()
+console.log(uppercase)
+var squareroot=Math.sqrt(25)
+console.log(squareroot)
+var maximum= Math.min(15,10,34,29)
+console.log(maximum)
+var power=Math.pow(10,4)
+console.log(power)
+console.log("My First Console Project")
+console.log("NaysaMishra")
+var a= 10
+var b= 5
+var add= a+b
+console.log(add)
+var sub= a-b
+console.log(sub)
+var multi= a*b
+console.log(multi)
+var div= a/b
+console.log(div)
